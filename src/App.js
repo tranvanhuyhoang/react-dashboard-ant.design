1ae9b8b21a0dd6b6170c18834ad45ab3ad6549e6
hoangtran <tranvanhuyhoang@gmail.com>
2021-04-26 17:44:36 +0700

    clear cache after build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, {Suspense} from 'react';
 import routes from './routes';
-export default class SiderDemo extends React.Component {
-  render() {
-    return (
-      <Suspense fallback={<div>Loading...</div>}>{routes}</Suspense>
-    );
-  }
+import withClearCache from "./clearCache";
+
+
+const ClearCacheComponent = withClearCache(MainApp);
+
+function App() {
+  return <ClearCacheComponent />;
 }
+
+function MainApp() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>{routes}</Suspense>
+  );
+}
+
+export default App;
