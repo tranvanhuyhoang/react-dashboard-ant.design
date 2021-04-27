4721062fbdd6e1f865a7f214b1d9cf67a81841e7
hoangtran <tranvanhuyhoang@gmail.com>
2021-04-28 00:04:49 +0700

    fix import react

--- src/pages/manageLesson/addLesson.js
+++ src/pages/manageLesson/addLesson.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Modal, Button } from 'antd';
 
-import React, { Component } from 'react';
 
 export default class ModalAddLesson extends Component {
   render() {
--- src/pages/manageStudents/addStudent.js
+++ src/pages/manageStudents/addStudent.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Modal, Button } from 'antd';
 
-import React, { Component } from 'react';
-
 export default class ModalAddStudent extends Component {
   render() {
     return (
